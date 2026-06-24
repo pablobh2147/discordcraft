2e5b8542676375a564f9337a36feda57aa322fca
pablobh2147 <github@pablobh.com>
2026-06-24 13:51:10 +0200

    Updated version to 2.0.0 and changed description

--- build.gradle.kts
+++ build.gradle.kts
@@ -3,12 +3,12 @@ plugins {
 }
 
 group = "com.pablobh"
-version = "1.0.0"
-description = "Discord integration plugin for Minecraft"
+version = "2.0.0"
+description = "Discord integration plugin and mod for Minecraft"
 
 allprojects {
     group = "com.pablobh"
-    version = "1.0.0"
+    version = "2.0.0"
 
     repositories {
         mavenCentral()
