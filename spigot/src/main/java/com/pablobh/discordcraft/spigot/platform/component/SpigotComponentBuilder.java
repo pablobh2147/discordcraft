4f18512ae764470b8271448e965d2cba95b52d19
pablobh2147 <github@pablobh.com>
2026-06-25 14:53:57 +0200

    Add MIT license headers to all Java source files

--- spigot/src/main/java/com/pablobh/discordcraft/spigot/platform/component/SpigotComponentBuilder.java
+++ spigot/src/main/java/com/pablobh/discordcraft/spigot/platform/component/SpigotComponentBuilder.java
@@ -1,6 +1,32 @@
+/*
+ * This file is part of DiscordCraft.
+ *
+ * Copyright (c) 2025 Pablo Bermejo Hernández
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in all
+ * copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+ * SOFTWARE.
+ */
+
 package com.pablobh.discordcraft.spigot.platform.component;
 
 import javax.annotation.Nonnull;
+
+import org.bukkit.ChatColor;
 
 import com.pablobh.discordcraft.platform.component.ClickAction;
 import com.pablobh.discordcraft.platform.component.MinecraftComponent;
@@ -8,7 +34,6 @@ import com.pablobh.discordcraft.platform.component.MinecraftComponentBuilder;
 
 import net.md_5.bungee.api.chat.ClickEvent;
 import net.md_5.bungee.api.chat.ComponentBuilder;
-import org.bukkit.ChatColor;
 
 public class SpigotComponentBuilder implements MinecraftComponentBuilder {
 
